42a3955f43ba81a1f8a55b4bb95be9026954f746
majun36 <majun1@jd.com>
2018-04-04 18:08:23 +0800

    updates

---
+++ built/index.js
@@ -0,0 +1,7 @@
+// 
+const add = (a, b) => {
+    return a + b;
+};
+function abc(params) {
+    console.log(params);
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// 
+const add = (a, b) => {
+  return a + b
+}
+
+function abc(params) {
+  console.log(params)
+}
